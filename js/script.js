0d21b54715af54976231e1325f3c4f4696d11d84
ladykakki <greta_calamari@hotmail.it>
2022-03-28 15:52:14 +0200

    multipli di 3 e 5 buzz e fizz

--- js/script.js
+++ js/script.js
@@ -6,20 +6,24 @@ for (let a = 1; a <= 100; a++){     console.log(a);
         const fizz = document.createElement('div');
         fizz.innerHTML = 'fizz';
         conta.append(fizz)
-    }
-
-
-
-        
-
-        
-
-
-
-
-
+    }else if(a % 5 === 0){
+        const buzz = document.createElement('div');
+        buzz.innerHTML = 'buzz';
+        conta.append(buzz)
+    }else(a)
 
 }
+
+
+        
+
+        
+
+
+
+
+
+
 
 
 
