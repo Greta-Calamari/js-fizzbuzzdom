3328c9d7b44a898b4f156ee78c979e359383435d
ladykakki <greta_calamari@hotmail.it>
2022-03-28 15:27:00 +0200

    ...

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
+let contaCento = 100;
+
+
+for (let a = 1; a <= 100; a++){     console.log(a);
+    if(a % 3 === 0) {
+        const fizz = document.createElement('div');
+        fizz.innerHTML = 'fizz';
+        conta.append(fizz)
+    }
+
+
+
+        
+
+        
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
  
 
- for (let a = 1; a <= 100; a++){     console.log(a);
- }
 
 
