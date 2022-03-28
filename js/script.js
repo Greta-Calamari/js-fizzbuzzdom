36607902a2d773227a94a15821ccc45c472b9bed
ladykakki <greta_calamari@hotmail.it>
2022-03-28 17:29:22 +0200

    ..

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ conta.append(row);
 for (let a = 1; a <= 100; a++){     
     const col = document.createElement('div');
     col.setAttribute('class', "col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2");
-    const card = document.createElement('div');
+    
     
     
     if(a % 3 === 0 && a % 5 === 0) {
