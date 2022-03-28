6c5eb7762b2ffae5a41599705417520f80ea551b
ladykakki <greta_calamari@hotmail.it>
2022-03-28 16:14:28 +0200

    ...

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 let contaCento = 100;
+const conta = document.getElementById('conta');
+const row = document.createElement('div');
 
 
-for (let a = 1; a <= 100; a++){     console.log(a);
+
+row.setAttribute('class', 'row g-3 justify-content-center');
+conta.append(row);
+
+
+
+for (let a = 1; a <= 100; a++){     
+    const col = document.createElement('div');
+    col.setAttribute('class', "col-12 col-sm-6 col-md-4 col-lg-1");
+    col.innerHTML = a ;
+    console.log(a);
     if(a % 3 === 0) {
         const fizz = document.createElement('div');
         fizz.innerHTML = 'fizz';
@@ -11,6 +23,9 @@ for (let a = 1; a <= 100; a++){     console.log(a);
         buzz.innerHTML = 'buzz';
         conta.append(buzz)
     }else(a)
+
+
+    row.append(col);
 
 }
 
